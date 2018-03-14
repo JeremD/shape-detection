30cbacbc8a052b4f4a6363c6eaa3f7a094350e04
Morgan Garralon <momomo@momomo.local>
2018-03-14 15:43:52 +0100

    structure

---
+++ Detection.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * User: jerem
+ * Date: 14/03/2018
+ */
+
+class Detection {
+    private $rightAnglesNb;
+    private $parallelSidesNb;
+    private $sidesNb;
+    private $identicalSidesNb;
+    private const SHAPES = [
+        "trq"   => "Ceci est un triangle quelconque",
+        "trr"   => "Ceci est un triangle rectangle",
+        "tri"   => "Ceci est un triangle isocèle",
+        "trri"  => "Ceci est un triangle rectangle isocèle",
+        "tre"   => "Ceci est un triangle équilatérale",
+        "quq"   => "Ceci est un quadrilatère quelconque",
+        "tra"   => "Ceci est un trapèze",
+        "par"   => "Ceci est un parallélogramme",
+        "los"   => "Ceci est un losange",
+        "rec"   => "Ceci est un rectangle",
+        "peq"   => "Ceci est un pentagone quelconque",
+        "per"   => "Ceci est un pentagone équilatérale",
+        "pe_"   => "Ceci est un pentagone",
+        "err"   => "Ceci est une forme incohérente"
+];
+
+    /**
+     * Detection constructor.
+     */
+    public function __construct($inputs) {
+        if(is_null($inputs) || sizeof($inputs) === 0) {
+            throw new Exception("The size of the input array was wrongly initialized!");
+        } else {
+            $this->input_nb = sizeof($inputs[0]->getDigitalDisplay());
+            $this->weights = array();
+            $this->deltas = array();
+        }
+    }
+
+    public function initWeights() {
+        for($i=0; $i < $this->input_nb; $i++) {
+            $this->weights[$i] = rand(-1000, 1000);
+        }
+    }
+
+    public function aggregate($inputs) {
+        $this->output = 0;
+
+        if(sizeof($inputs) != sizeof($this->weights)) {
+            throw new Exception("The size of the input array is different from the weight array!");
+        } else {
+            foreach($inputs as $key => $value) {
+                $this->inputs[$key] = $value;
+                $this->output += $value * $this->weights[$key];
+            }
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getInputs()
+    {
+        return $this->inputs;
+    }
+
+    /**
+     * @param mixed $inputs
+     */
+    public function setInputs($inputs)
+    {
+        $this->inputs = $inputs;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getInputNb()
+    {
+        return $this->input_nb;
+    }
+
+    /**
+     * @param mixed $input_nb
+     */
+    public function setInputNb($input_nb)
+    {
+        $this->input_nb = $input_nb;
+    }
+
+    /**
+     * @return array
+     */
+    public function getDeltas()
+    {
+        return $this->deltas;
+    }
+
+    /**
+     * @param array $deltas
+     */
+    public function setDeltas(array $deltas)
+    {
+        $this->deltas = $deltas;
+    }
+
+    /**
+     * @return array
+     */
+    public function getWeights()
+    {
+        return $this->weights;
+    }
+
+    /**
+     * @param array $weights
+     */
+    public function setWeights($weights)
+    {
+        $this->weights = $weights;
+    }
+    /**
+     * @return mixed
+     */
+    public function getOutput()
+    {
+        return $this->output;
+    }
+
+    /**
+     * @param mixed $output
+     */
+    public function setOutput($output)
+    {
+        $this->output = $output;
+    }
+}
---
+++ index.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * User: jerem & momomo
+ * Date: 14/03/2018
+ */
+
+require_once('Detection.php');
+
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="img/favicon.png" type="image/png">
+    <link rel="stylesheet" type="text/css" href="style/perceptron_style.css">
+    <script type='text/javascript' src='https://code.jquery.com/jquery-latest.min.js'></script>
+    <script>
+        $(document).ready(function() {
+            $('input[name=input]').change(function(){
+                $('form[name=input_form]').submit();
+            });
+        });
+    </script>
+    <title>Neural network project: a perceptron detecting some digits</title>
+</head>
+<body>
+    <div>
+        <h1>Neural network project<span class="smaller"> a perceptron detecting some digits</span></h1>
+        <hr/>
+        <hr/>
+        <div>
+            <h2>Training section</h2>
+            <hr/>
+            <form action="." method="post">
+                Set the number of learnin' iterations:
+                <p>
+                    <input type="number" name="iteration_nb" value="1000">
+                </p>
+                Wanna try an automatic training?
+                <p>
+                    <input type="checkbox" name="automatic_training" value="1">
+                </p>
+                <input type="submit" name="training" value="Train & learn">
+            </form>
+        </div>
+        <hr/>
+        <div>
+            <h2>Guessing section</h2>
+            <hr/>
+            <form action="." method="post" name="input_form">
+                Make a wish:
+                <p>
+                    <div class="cc-selector">
+                        <input id="zero" type="radio" name="input" value="zero" />
+                        <label class="drinkcard-cc zero" for="zero"></label>
+                        <input id="one" type="radio" name="input" value="1" />
+                        <label class="drinkcard-cc one" for="one"></label>
+                        <input id="two" type="radio" name="input" value="2" />
+                        <label class="drinkcard-cc two" for="two"></label>
+                        <input id="three" type="radio" name="input" value="3" />
+                        <label class="drinkcard-cc three" for="three"></label>
+                        <input id="four" type="radio" name="input" value="4" />
+                        <label class="drinkcard-cc four" for="four"></label>
+                        <input id="five" type="radio" name="input" value="5" />
+                        <label class="drinkcard-cc five" for="five"></label>
+                        <input id="six" type="radio" name="input" value="6" />
+                        <label class="drinkcard-cc six" for="six"></label>
+                        <input id="seven" type="radio" name="input" value="7" />
+                        <label class="drinkcard-cc seven" for="seven"></label>
+                    </div>
+                </p>
+            </form>
+        </div>
+        <hr/>
+        <div>
+            <h2>Reset section</h2>
+            <hr/>
+            <form action="." method="post">
+                Reset the whole perceptron:
+                <p>
+                    <input type="reset" value="Reset">
+                    <input type="submit" value="Detect!">
+                </p>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
